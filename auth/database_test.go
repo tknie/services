1c522218a5b0880fefecb71039763c9ea6fea9c6
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2023-10-21 21:52:54 +0200

    Add copyright and db tag

--- auth/database_test.go
+++ auth/database_test.go
@@ -1,3 +1,17 @@
+//go:build database_env_active
+// +build database_env_active
+
+/*
+* Copyright 2022-2023 Thorsten A. Knieling
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+ */
+
 package auth
 
 import (
