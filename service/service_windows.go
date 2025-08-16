f8897a09bc0a57631294565da065d2754d4c3428
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2025-08-16 16:38:11 +0200

    Update copyright year

--- service/service_windows.go
+++ service/service_windows.go
@@ -2,7 +2,7 @@
 // +build windows
 
 /*
-* Copyright 2022-2024 Thorsten A. Knieling
+* Copyright 2022-2025 Thorsten A. Knieling
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -19,6 +19,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/tknie/log"
+	"github.com/tknie/services"
 	"golang.org/x/sys/windows/svc"
 	"golang.org/x/sys/windows/svc/debug"
 	"golang.org/x/sys/windows/svc/eventlog"
