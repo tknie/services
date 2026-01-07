a1a47bd2a97db0927afe8ed122430c70a885d4a5
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2026-01-07 20:48:05 +0100

    Copyright update 2026

--- pid_unix.go
+++ pid_unix.go
@@ -2,7 +2,7 @@
 // +build aix darwin dragonfly freebsd linux netbsd openbsd solaris
 
 /*
-* Copyright 2022-2025 Thorsten A. Knieling
+* Copyright 2022-2026 Thorsten A. Knieling
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
